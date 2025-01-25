900d06a13da33af1c2fc18146ffbb46048a1f648
joerg <jkuhlm@web.de>
2025-01-25 19:00:22 +0100

    Komm mit

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : hello.c
  Author      : jokubokla
- Version     : 0.9.2
+ Version     : 0.9.3
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
